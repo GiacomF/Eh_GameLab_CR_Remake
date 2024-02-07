50331a6ac5a0a64c9a83447dca3971ac483510cd
GiacomF <giacomofiumana@gmail.com>
2024-02-07 13:54:24 +0100

    Project Setup & Level Generator
    Generator needs some more tweaks

---
+++ Assets/Generate_Random_Path.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Generate_Random_Path : MonoBehaviour
+{
+    public List<GameObject> SafeBlocks;
+    public List<GameObject> UnsafeBlocks;
+    List<bool> Safe = new List<bool>();
+    public float InitialPercentage;
+    public int LevelThreshold;
+    public int MaxRoad; 
+
+    void Start()
+    {
+        GenerateList(InitialPercentage, LevelThreshold, MaxRoad);
+    }
+    public void GenerateList(float InitialPercentage, int LevelThreshold, int MaxRoad)
+    {
+        for(int i = 0; i < MaxRoad; i++)
+        {
+            float GeneratePercentage = UnityEngine.Random.value;
+            Safe.Add(GeneratePercentage < InitialPercentage - MathF.Truncate(i/LevelThreshold) * 0.05f);
+            Vector3 NewBLockPosition = new Vector3(0, 0, i);
+            GameObject RandomObject;
+
+            if(Safe[i])
+            {
+                RandomObject = SafeBlocks[UnityEngine.Random.Range(0, SafeBlocks.Count)];
+            }
+            else
+            {
+                RandomObject = UnsafeBlocks[UnityEngine.Random.Range(0, UnsafeBlocks.Count)];
+            }
+
+            GameObject SpawnedObject = GameObject.Instantiate(RandomObject);
+            SpawnedObject.transform.position = NewBLockPosition;
+        }
+    }
+}
