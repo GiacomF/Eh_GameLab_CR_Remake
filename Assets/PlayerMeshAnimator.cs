a73ff993b7780b1f12514056e1db20acfdc45d14
GiacomF <giacomofiumana@gmial.com>
2024-02-13 17:49:35 +0100

    Character Movememtn Tweaks

--- Assets/PlayerMeshAnimator.cs
+++ Assets/PlayerMeshAnimator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.ReorderableList.Element_Adder_Menu;
 using UnityEngine;
 
 public class PlayerMeshAnimator : MonoBehaviour
@@ -13,9 +14,47 @@ public class PlayerMeshAnimator : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W))
+        PrepareJump();
+        Jump();
+    }
+
+    void Jump()
+    {
+        if(Input.GetKeyUp(KeyCode.W))
         {
             playerAnimation.SetTrigger("Hopping");
+        }
+        else if(Input.GetKeyUp(KeyCode.S))
+        {
+            playerAnimation.SetTrigger("Hopping");
+        }
+        else if(Input.GetKeyUp(KeyCode.A))
+        {
+            playerAnimation.SetTrigger("Hopping");
+        }
+        else if(Input.GetKeyUp(KeyCode.D))
+        {
+            playerAnimation.SetTrigger("Hopping");
+        }
+    }
+
+    void PrepareJump()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            playerAnimation.SetTrigger("Preparing");
+        }
+        else if(Input.GetKeyDown(KeyCode.S))
+        {
+            playerAnimation.SetTrigger("Preparing");
+        }
+        else if(Input.GetKeyDown(KeyCode.A))
+        {
+            playerAnimation.SetTrigger("Preparing");
+        }
+        else if(Input.GetKeyDown(KeyCode.D))
+        {
+            playerAnimation.SetTrigger("Preparing");
         }
     }
 }
