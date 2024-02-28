438ceb49b32967ae9e3cfb3f66dc70d72cdd656c
Lorenzo Camanzi <lorenzo.camanzi@gmail.com>
2024-02-28 14:27:29 +0100

    Riprese modifiche UI and Camera

--- Assets/Scripts/Player/AnimatorController.cs
+++ Assets/Scripts/Player/AnimatorController.cs
@@ -13,7 +13,10 @@ public class AnimatorController : MonoBehaviour
     }
 
     void Update()
-    {   
+    {
+
+        if (GameStateManager.instance.gameIsPaused) return;
+
         if(playerController.jumpStart)
         {
             animator.SetBool("jumpStart", true);
