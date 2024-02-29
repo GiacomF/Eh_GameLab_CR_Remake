181d596ddc349f379a426569bd97111b24bcd2ce
Lorenzo Camanzi <lorenzo.camanzi@gmail.com>
2024-03-01 00:07:55 +0100

    piu pillars

--- Assets/DestroyableObject.cs
+++ Assets/DestroyableObject.cs
@@ -5,39 +5,42 @@ using UnityEngine;
 public class DestroyableObject : MonoBehaviour
 {
     public AudioClip groundBreaking;
+    
     public bool mustSplit = false;
-    public float timer = 4f;
+    public float timeToSplit = 1f;
+    
     public GameObject half1;
     public GameObject half2;
-    public GameObject coll;
-    Vector3 offset = new Vector3(0.25f, 0, 0);
-    void Update()
-    {
-        if(mustSplit)
-        {
-            timer -= Time.deltaTime;
-            Debug.Log(timer);
+    
+    public BoxCollider coll;
 
-            if(timer <= 0)
-            {
-                Split();
-                mustSplit = false;
-            }
-        }
-    }
+    private bool isBreaking = false;
+
+    Vector3 offset = new Vector3(0.25f, 0, 0);
+
     void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
-            mustSplit = true;
+            if (!isBreaking)
+            { 
+                StartCoroutine(waitAndSplit());
+            }
             AudioSource.PlayClipAtPoint(groundBreaking, transform.position);
         }
     }
 
     void Split()
     {
+        Object.Destroy(coll);
         half1.transform.position -= offset;
         half2.transform.position += offset;
-        coll.SetActive(false);
+    }
+
+    private IEnumerator waitAndSplit() 
+    {
+        isBreaking = true;
+        yield return new WaitForSeconds(timeToSplit);
+        Split();
     }
 }
